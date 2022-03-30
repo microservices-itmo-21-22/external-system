4f6291ad5c9a20cf60b704b028d36f1fd9a5de74
afterbvrner <maksim.golish2002@gmail.com>
2022-03-31 00:39:44 +0300

    chore: Add logs

--- src/main/kotlin/ru/itmo/tps/service/core/TransactionHandler.kt
+++ src/main/kotlin/ru/itmo/tps/service/core/TransactionHandler.kt
@@ -1,11 +1,15 @@
 package ru.itmo.tps.service.core
 
+import com.itmo.microservices.commonlib.annotations.InjectEventLogger
+import com.itmo.microservices.commonlib.logging.EventLogger
 import lombok.RequiredArgsConstructor
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import ru.itmo.tps.dto.Transaction
 import ru.itmo.tps.dto.TransactionRequest
 import ru.itmo.tps.dto.TransactionStatus
 import ru.itmo.tps.dto.management.Account
+import ru.itmo.tps.event.NotableEvents
 import ru.itmo.tps.exception.EntityNotFoundException
 import ru.itmo.tps.exception.NotAuthenticatedException
 import ru.itmo.tps.service.core.handlestrategy.TransactionHandlingStrategy
@@ -18,11 +22,19 @@ class TransactionHandler(
     private val accountService: AccountService,
     private val transactionHandlingStrategies: List<TransactionHandlingStrategy>
 ) {
+
+    @InjectEventLogger
+    private lateinit var eventLogger: EventLogger
+    private val log = KotlinLogging.logger {}
+
+
     suspend fun submitTransaction(transactionRequest: TransactionRequest): Transaction {
+        eventLogger.info(NotableEvents.I_TRANSACTION_PROCESSING_STARTED, transactionRequest.clientSecret)
         val account: Account
         try {
             account = accountService.findByClientSecret(transactionRequest.clientSecret)
         } catch (e: EntityNotFoundException) {
+            log.warn { "Account for client secret '${transactionRequest.clientSecret}' not found" }
             throw NotAuthenticatedException("Cannot find account with given client secret")
         }
 
@@ -35,6 +47,8 @@ class TransactionHandler(
 
         transaction = selectStrategy(account).handle(transaction, account)
 
+        if (transaction.status == TransactionStatus.SUCCESS)
+            eventLogger.info(NotableEvents.I_TRANSACTION_SUCCEEDED, transaction.id)
         return transaction
     }
 
