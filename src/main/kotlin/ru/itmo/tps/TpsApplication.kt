8c2ef4edc593d7f6d023c4cf336505157aba5dd3
DebDoDab <debbius3@gmail.com>
2023-03-03 15:57:12 +0300

    chores: remove extra empty lines

--- src/main/kotlin/ru/itmo/tps/TpsApplication.kt
+++ src/main/kotlin/ru/itmo/tps/TpsApplication.kt
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
-
-
 @OpenAPIDefinition(
     servers = [Server(url = "/", description="Your host")]
 )
