806a4290d0fd645d0265f276d74aa055444713d0
DebDoDab <debbius3@gmail.com>
2023-03-03 15:48:03 +0300

    fix: swagger server url

--- src/main/kotlin/ru/itmo/tps/TpsApplication.kt
+++ src/main/kotlin/ru/itmo/tps/TpsApplication.kt
@@ -1,9 +1,16 @@
 package ru.itmo.tps
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
+
+
+@OpenAPIDefinition(
+    servers = [Server(url = "/", description="Your host")]
+)
 @SpringBootApplication
 @EnableCaching
 class TpsApplication
